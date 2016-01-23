15619bdd2fb0350bda4585872598e704896edfeb
Collin Guarino <collin.guarino@gmail.com>
2016-01-22 20:10:24 -0500

    Inner struct for bridge info from /description.html

--- bridge.go
+++ bridge.go
@@ -12,17 +12,15 @@ type Bridge struct {
     IPAddress   string
     Username    string
     Debug       bool
-    Info        Description
-}
-
-type Description struct {
-    DeviceType          string
-    FriendlyName        string
-    ManufacturerURL     string
-    About               string
-    ModelName           string
-    ModelNumber         string
-    ModelURL            string
-    SerialNumber        string
-    UDN                 string
+    Info        struct {
+        DeviceType          string
+        FriendlyName        string
+        ManufacturerURL     string
+        About               string
+        ModelName           string
+        ModelNumber         string
+        ModelURL            string
+        SerialNumber        string
+        UDN                 string
+    }
 }
