8392337305e5d5a3bb287ec62f212a8b64df95fb
Collin Guarino <collin.guarino@gmail.com>
2016-01-24 23:49:37 -0500

    Unit tests for lights.go file.

--- lights_test.go
+++ lights_test.go
@@ -1,11 +1,10 @@
 package hue
 
 import (
-    "log"
     "testing"
 )
-
-func TestNewBridge(t *testing.T) {
+    
+func TestGetAllLights(t *testing.T) {
     bridge := NewBridge("192.168.1.128", "319b36233bd2328f3e40731b23479207")
-    log.Println(bridge)
+    GetAllLights(bridge)
 }
