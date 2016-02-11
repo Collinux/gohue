4385cb7d12c5c722ac617e9232a7a2937eef1c15
Collin Guarino <collin.guarino@gmail.com>
2016-02-10 20:04:27 -0500

    Added email to license headers.

--- light.go
+++ light.go
@@ -1,7 +1,7 @@
 /*
 * light.go
 * GoHue library for Philips Hue
-* Copyright (C) 2016 Collin Guarino (Collinux)
+* Copyright (C) 2016 Collin Guarino (Collinux) collin.guarino@gmail.com
 * License: GPL version 2 or higher http://www.gnu.org/licenses/gpl.html
 */
 
