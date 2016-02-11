acb88f00e8e9acf7821efd80017a39e1d6620350
Collin Guarino <collin.guarino@gmail.com>
2016-02-10 19:55:02 -0500

    Added GPLv2 license header to all files.

--- bridge_test.go
+++ bridge_test.go
@@ -1,3 +1,10 @@
+/*
+* bridge_test.go
+* GoHue library for Philips Hue
+* Copyright (C) 2016 Collin Guarino
+* License: GPL version 2 or higher http://www.gnu.org/licenses/gpl.html
+*/
+
 package hue
 
 import (
--- light.go
+++ light.go
@@ -1,5 +1,11 @@
-// http://www.developers.meethue.com/documentation/lights-api
+/*
+* light.go
+* GoHue library for Philips Hue
+* Copyright (C) 2016 Collin Guarino
+* License: GPL version 2 or higher http://www.gnu.org/licenses/gpl.html
+*/
 
+// http://www.developers.meethue.com/documentation/lights-api
 package hue
 
 import (
