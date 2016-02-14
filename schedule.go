53017599b1f6fb4fd85e78bb5167da5a565387d3
Collin Guarino <collin.guarino@gmail.com>
2016-02-14 00:23:00 -0500

    Base implemention for schedule.go with structs.

---
+++ schedule.go
@@ -0,0 +1,42 @@
+/*
+* schedule.go
+* GoHue library for Philips Hue
+* Copyright (C) 2016 Collin Guarino (Collinux) collin.guarino@gmail.com
+* License: GPL version 2 or higher http://www.gnu.org/licenses/gpl.html
+*/
+
+package hue
+
+import (
+    //"fmt"
+)
+
+type Timer struct {
+    Index       int
+    Name        string      `json:"name",omitempty`
+    Description string      `json:"description",omitempty`
+    Command     CommandInfo `json:"command",omitempty`
+    Time        string      `json:"time",omitempty`
+    Created     string      `json:"created",omitempty`
+    Status      string      `json:"status",omitempty`
+    AutoDelete  bool        `json:"autodelete",omitempty`
+    StartTime   string      `json:"starttime",omitempty`
+}
+
+type Alarm struct {
+    Index       string
+    Name        string      `json:"name",omitempty`
+    Description string      `json:"description",omitempty`
+    Command     CommandInfo `json:"command",omitempty`
+    LocalTime   string      `json:"localtime",omitempty`
+    Time        string      `json:"time",omitempty`
+    Created     string      `json:"created",omitempty`
+    Status      string      `json:"status",omitempty`
+    AutoDelete  bool        `json:"autodelete",omitempty`
+}
+
+type CommandInfo struct {
+    Address     string      `json:"address",omitempty`
+    Body        string      `json:"body",omitempty` // TODO: may be diff type
+    Method      string      `json:"method",omitempty`
+}
